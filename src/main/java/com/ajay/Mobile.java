3c83c5ab5b454fa3683531468d7485c0de329d78
Vijay_9524 <omvijay44>
2022-01-23 18:19:57 -0800

    all examples

--- src/main/java/com/ajay/Mobile.java
+++ src/main/java/com/ajay/Mobile.java
@@ -1,12 +1,12 @@
 package com.ajay;
 
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Mobile {
 
     public static void main(String[] args) {
-
         ApplicationContext applicationContext=new ClassPathXmlApplicationContext("spring1.xml");
         Sim sim=(Sim) applicationContext.getBean("sim");
         sim.calling();
